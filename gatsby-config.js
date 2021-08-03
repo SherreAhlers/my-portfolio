02ced08a5c82181064c72b5eea181f2fa380d91a
Sherre Ahlers <sherre.ahlers@gmail.com>
2021-08-03 14:41:52 -0500

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ const path = require("path")
 module.exports = {
   siteMetadata: {
     title: "Sherre-Ahlers-Portfolio",
+    siteUrl: "https://sherreportfolio.gatsbyjs.io/",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
